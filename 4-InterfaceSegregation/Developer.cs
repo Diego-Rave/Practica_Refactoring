1afa9f9d154da9b6245632b1d4f0b68afedeed9e
Diego Rave <dagil99@soy.sena.edu.co>
2026-05-25 17:36:47 -0500

    Aplicación del principio de segregación de Interfaz

--- 4-InterfaceSegregation/Developer.cs
+++ 4-InterfaceSegregation/Developer.cs
@@ -1,6 +1,6 @@
 namespace InterfaceSegregation
 {
-    public class Developer : IActivities
+    public class Developer : IWorkTeamActivities, IDevelopActivities
     {
         public Developer()
         {
@@ -16,19 +16,15 @@ namespace InterfaceSegregation
             throw new ArgumentException();
         }
 
-        public void Design() 
-        {
-            throw new ArgumentException();
-        }
-
         public void Develop() 
         {
             Console.WriteLine("I'm developing the functionalities required");
         }
 
-        public void Test() 
+        public void Design() 
         {
-            throw new ArgumentException();
+            Console.WriteLine("I'm designing new futures");
         }
+
     }
 }
